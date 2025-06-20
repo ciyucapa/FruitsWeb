b1652ae56aaf4966cc2309714e5b771cb6546173
cindy caceres <cindycaceres134@gmail.com>
2025-06-20 17:00:30 -0500

    [ADD]: styles card

--- src/components/FruitCard/index.tsx
+++ src/components/FruitCard/index.tsx
@@ -1,5 +1,6 @@
 import type { Fruit } from '../../utils/interfaces';
 import { useTakeImage } from '../../hooks/useTakeImage';
+import './index.css';
 
 type FruitCardProps = {
   fruit: Fruit;
@@ -9,17 +10,53 @@ const FruitCard: React.FC<FruitCardProps> = ({ fruit }) => {
   const { imgSrc, handleImageError } = useTakeImage(fruit.name);
 
   return (
-    <div className="card h-100">
+    <div className="card fruit-card">
       <img
         src={imgSrc}
         onError={handleImageError}
         alt={fruit.name}
         className="card-img-top"
       />
-      <div className="card-body">
-        <h5 className="card-title">{fruit.name}</h5>
-        <p className="card-text">Calories: {fruit.nutritions.calories}</p>
-        <p className="card-text">Sugar: {fruit.nutritions.sugar}</p>
+      <div className="card-body p-4 text-start">
+        <h5 className="card-title text-gold-24">{fruit.name}</h5>
+        <div className="row text-center">
+          <div className="col text-start">
+            <p className='text-gold-16 fw-bold' >Family:</p>
+            <p className='text-gold-16 fw-normal' >{fruit.family}</p>
+          </div>
+          <div className="col text-start">
+            <p className='text-gold-16 fw-bold'>Order:</p>
+            <p className='text-gold-16 fw-normal' >{fruit.order}</p>
+          </div>
+          <div className="col text-start">
+            <p className='text-gold-16 fw-bold'>Genus:</p>
+            <p className='text-gold-16 fw-normal' >{fruit.genus}</p>
+          </div>
+
+        </div>
+        <div>
+          <p className="text-gold-18">Nutritions</p>
+          <div>
+            <p className="card-text">Calories:</p>
+            <p className="card-text">{fruit.nutritions.calories}</p>
+          </div>
+          <div>
+            <p className="card-text">Fat:</p>
+            <p className="card-text">{fruit.nutritions.fat}</p>
+          </div>
+          <div>
+            <p className="card-text">Sugar:</p>
+            <p className="card-text">{fruit.nutritions.sugar}</p>
+          </div>
+          <div>
+            <p className="card-text">Carbohydrates:</p>
+            <p className="card-text">{fruit.nutritions.carbohydrates}</p>
+          </div>
+          <div>
+            <p className="card-text">Protein:</p>
+            <p className="card-text">{fruit.nutritions.protein}</p>
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/components/ListOfFruitCards/index.tsx
+++ src/components/ListOfFruitCards/index.tsx
@@ -1,18 +1,36 @@
-import FruitCard from "../FruitCard";
+import { useState } from 'react';
+import FruitCard from '../FruitCard';
 import type { FruistProps } from '../../utils/interfaces';
 
-
 const ListOfFruitCards: React.FC<FruistProps> = ({ fruits }) => {
-  console.log("datos 1:", fruits);
+  const [visibleCount, setVisibleCount] = useState(9);
+
+  const handleSeeMore = () => {
+    setVisibleCount((prev) => prev + 3); // Mostrar 3 frutas más
+  };
+
+  const visibleFruits = fruits.slice(0, visibleCount);
+
+  console.log("datosFruit:", fruits)
 
   return (
-    <div className="row">
-      <h2>Listado de frutas</h2>
-      {fruits.map((fruit) => (
-        <div key={fruit.id} className="col-md-3 mb-4">
-          <FruitCard fruit={fruit} />
+    <div className="container">
+      <h2 className="my-4">Listado de frutas</h2>
+      <div className="row">
+        {visibleFruits.map((fruit) => (
+          <div key={fruit.id} className="col-md-4 mb-4">
+            <FruitCard fruit={fruit} />
+          </div>
+        ))}
+      </div>
+
+      {visibleCount < fruits.length && (
+        <div className="text-center">
+          <button className="btn btn-primary" onClick={handleSeeMore}>
+            See more
+          </button>
         </div>
-      ))}
+      )}
     </div>
   );
 };
