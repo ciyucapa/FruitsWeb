6327ffe506663dcafd271d07e63e0c9a2c402b23
cindy caceres <cindycaceres134@gmail.com>
2025-06-21 09:02:42 -0500

    [ADD]: styles input search

--- src/components/FilterAndSortedFruits/index.tsx
+++ src/components/FilterAndSortedFruits/index.tsx
@@ -23,7 +23,7 @@ const FilterAndSortedFruits: React.FC<Props> = ({
   return (
     <div className="d-flex align-items-center gap-3 flex-wrap justify-content-end filter-bar ">
       <select
-        className="form-select w-auto"
+        className="form-select select-rounded"
         value={filterType}
         onChange={(e) => onFilterTypeChange(e.target.value as FilterType)}
       >
@@ -32,13 +32,18 @@ const FilterAndSortedFruits: React.FC<Props> = ({
         <option value="genus">Genus</option>
       </select>
 
-      <input
-        type="text"
-        className="form-control w-auto"
-        placeholder={`Search by ${filterType}`}
-        value={filterValue}
-        onChange={(e) => onFilterValueChange(e.target.value)}
-      />
+      <div className="input-rounded">
+        <input
+          type="text"
+          className="form-control border-0"
+          placeholder={`Search by ${filterType}`}
+          value={filterValue}
+          onChange={(e) => onFilterValueChange(e.target.value)}
+        />
+        <span className="input-group-text border-0">
+          <i className="bi bi-search"></i>
+        </span>
+      </div>
 
       <button className="btn btn-outline-secondary custom-order-btn" onClick={onToggleSortOrder}>
         Order {sortOrder === 'asc' ? 'A - Z' : 'Z - A'}
