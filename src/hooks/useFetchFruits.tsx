9f3a6279d451375a60edc528270ac8e140951616
cindy caceres <cindycaceres134@gmail.com>
2025-06-21 13:15:35 -0500

    [ADD]: useFetch

--- src/hooks/useFetchFruits.tsx
+++ src/hooks/useFetchFruits.tsx
@@ -1,32 +1,32 @@
 import { useState, useEffect } from "react";
 import axios from 'axios';
 
-const API_URL = 'https://cors-anywhere.herokuapp.com/https://www.fruityvice.com/api/fruit/all';
+const isLocalhost = window.location.hostname === 'localhost';
 
+const API_URL = isLocalhost
+  ? 'https://cors-anywhere.herokuapp.com/https://www.fruityvice.com/api/fruit/all'
+  : 'https://www.fruityvice.com/api/fruit/all';
 
 const useFetchFruits = () => {
-    const [fruits, setFruits] = useState([]);
-    const [loading, setLoading] = useState(true);
+  const [fruits, setFruits] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-    useEffect(() => {
-        axios.get(API_URL)
-            .then(response => {
-                setFruits(response.data);
-                console.log("data1", response.data)
-                setLoading(false);
-            })
-            .catch(error => {
-                console.error("Error fetching fruits:", error);
-                setLoading(false);
-            });
-    }, []);
+  useEffect(() => {
+    axios.get(API_URL)
+      .then(response => {
+        setFruits(response.data);
+        setLoading(false);
+      })
+      .catch(error => {
+        console.error("Error fetching fruits:", error);
+        setLoading(false);
+      });
+  }, []);
 
-    console.log("dats2", fruits)
-
-    return {
-        fruits,
-        loading
-    }
+  return {
+    fruits,
+    loading
+  };
 };
 
 export default useFetchFruits;
